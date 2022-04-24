ff6ff041a00faf0713dd04b7c5a6808bb58522cc
willy4112 <93559065+willy4112@users.noreply.github.com>
2022-04-24 16:15:22 +0800

    Update color_map.py

--- color_map.py
+++ color_map.py
@@ -1,35 +1,123 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb  2 10:22:21 2022
-
-@author: willy
+Created on Sat Jan 22 16:16:20 2022
+@author: Chai-Wei Waang
 """
+#%% color_map 1
 
-import random
+import numpy as np
+import pylab as pl
+from matplotlib.colors import hsv_to_rgb
 
-List_number = [0,1,2,3,4,5,6,7,8,9]
-Answer = random.sample(List_number,4)
+V, H = np.mgrid[0:1:100j, 0:1:360j]
+S = np.ones_like(V)*1   # 設定0-1之間
+HSV = np.dstack((H,S,V))
+RGB = hsv_to_rgb(HSV)
+pl.imshow(RGB, origin="lower", extent=[0, 360, 0, 1], aspect=150)
+pl.xlabel("H")
+pl.ylabel("V")
+pl.title("$S_{HSV}=0.1$")
+pl.show()
 
-Enter = [int(n) for n in input('請輸入數字，若輸入0表示放棄:')]
+#%% color_map 2
 
-a = 0
-b = 0
-times = 1
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import colorsys
 
-while 1==1:
-    for i in range(4):
-        for j in range(4):
-            if Enter[j] == Answer[i] and i==j : a +=1
-            if Enter[j] == Answer[i] and i!=j : b +=1
-    print(a,'A',b,'B')
-        
-    if a == 4:
-        print('答案為', Answer,'共花了', times,'次')
-        break
-    Enter = [int(n) for n in input('請輸入數字，若輸入0表示放棄:')]
-    if Enter[0] == 0 and len(Enter)==1:
-        print('答案為', Answer,'已放棄')
-        break
-    a = 0
-    b = 0
-    times += 1
+
+X = []
+Y = []
+C = []
+
+number = 60
+s =  0.75         # 0.75-0.5
+
+table = np.ones((10,number))
+table = pd.DataFrame(table)
+for i in range(10):
+    for j in range(number):
+        v = round(i/10,2)
+        h = round(j/number,2)
+        # hsv = str((h,s,v))
+        (r,g,b) = colorsys.hsv_to_rgb(h,s,v)
+        r = hex(int(r*255))[2:].zfill(2)
+        g = hex(int(g*255))[2:].zfill(2)
+        b = hex(int(b*255))[2:].zfill(2)
+        rgb = "#"+r+g+b
+        table.iloc[i,j] = rgb
+        X.append(j)
+        Y.append(i)
+        C.append(rgb)
+
+# 畫成漸層表格
+# plt.figure(figsize=[10,3])
+# plt.scatter(X, Y, s=200, color = C, marker="s")
+# plt.title("$V_{HSV}=$"+str(v))
+# plt.show()
+
+
+# 畫成同心圓
+L = [1]*number
+
+# C0 = list(table.iloc[0,:])
+# C1 = list(table.iloc[1,:])
+# C2 = list(table.iloc[2,:])
+# C3 = list(table.iloc[3,:])
+# C4 = list(table.iloc[4,:])
+# C5 = list(table.iloc[5,:])
+# C6 = list(table.iloc[6,:])
+# C7 = list(table.iloc[7,:])
+# C8 = list(table.iloc[8,:])
+# C9 = list(table.iloc[9,:])
+
+# plt.pie(L, wedgeprops={'width': 0.50},colors=list(table.iloc[0,:]))
+# plt.pie(L, wedgeprops={'width': 0.45},colors=list(table.iloc[1,:]))
+# plt.pie(L, wedgeprops={'width': 0.40},colors=list(table.iloc[2,:]))
+# plt.pie(L, wedgeprops={'width': 0.35},colors=list(table.iloc[3,:]))
+# plt.pie(L, wedgeprops={'width': 0.30},colors=list(table.iloc[4,:]))
+# plt.pie(L, wedgeprops={'width': 0.25},colors=list(table.iloc[5,:]))
+# plt.pie(L, wedgeprops={'width': 0.20},colors=list(table.iloc[6,:]))
+# plt.pie(L, wedgeprops={'width': 0.15},colors=list(table.iloc[7,:]))
+# plt.pie(L, wedgeprops={'width': 0.10},colors=list(table.iloc[8,:]))
+plt.pie(L, wedgeprops={'width': 0.3},colors=list(table.iloc[9,:]))
+plt.title("$S_{HSV}=$"+str(s))
+plt.show()
+
+#%% 取出顏色
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import colorsys
+
+Number = 360
+S = 0.75
+V = 0.9
+start = 0
+color_number = 3
+
+
+color_list = np.ones((1,Number))
+color_list = pd.DataFrame(color_list)
+for i in range(Number):
+    H = round(i/Number,2)
+    (r,g,b) = colorsys.hsv_to_rgb(H,S,V)
+    r = hex(int(r*255))[2:].zfill(2)
+    g = hex(int(g*255))[2:].zfill(2)
+    b = hex(int(b*255))[2:].zfill(2)
+    rgb = "#"+r+g+b
+    color_list.iloc[0,i] = rgb
+
+
+X1 = []
+Y1 = [1]*color_number
+C1 = []
+
+for i in range(color_number):
+    X1.append(i)
+    location = (start%Number+int(Number/color_number)*i)%Number
+    C1.append(color_list.iloc[0,location])
+
+plt.pie(Y1,colors=C1)
